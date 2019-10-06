20ec2697a1f4dddd3c11f04fe96248400dff83d2
jesperberth <jesper.berth@arrow.com>
2019-10-06 21:51:16 +0200

    test

--- azure_class_playbooks/setup_users.ps1
+++ azure_class_playbooks/setup_users.ps1
@@ -23,8 +23,7 @@ function CreateUser {
 
     New-AzureADMSInvitation -InvitedUserDisplayName $name -InvitedUserEmailAddress $email -InviteRedirectURL https://portal.azure.com -SendInvitationMessage $true
     New-AzureRmResourceGroup -Name $rgname -Location $location
-    #New-AzureADGroup -DisplayName $rgname -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet"
-
+   
     $user = Get-AzureADUser -Filter "DisplayName eq '$name'" | Select-Object ObjectId
 
     New-AzureRmRoleAssignment -ObjectId $user.ObjectId -RoleDefinitionName Owner -Scope "/subscriptions/$subid.Subscription"
