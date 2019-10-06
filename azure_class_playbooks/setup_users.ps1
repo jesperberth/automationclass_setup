1c0c3bbfafe0a50a7eafbe78067ef92b3f3c080b
jesperberth <jesper.berth@arrow.com>
2019-10-06 20:40:50 +0200

    setup_users

--- azure_class_playbooks/setup_users.ps1
+++ azure_class_playbooks/setup_users.ps1
@@ -7,23 +7,20 @@ function CreateUser {
     $emailString = $emailStringTmp -replace "\.","_"
     write-host $emailString
     $rgname = $emailString
-    
-    #$uniqtext1 = 1..99 | Get-Random -Count 1
-    #$uniqtext2 = 1..99 | Get-Random -Count 1
-    #$sec = Get-Date -Second
-    #write-host $uniqtext2
-    #write-host $sec
-    #$uniqname = 
+    $rgname = $rgname.substring(0,16)
 
+    $storage = $emailString -replace "_",""
+
+    write-host $rgname
     New-AzureADMSInvitation -InvitedUserDisplayName $name -InvitedUserEmailAddress $email -InviteRedirectURL https://portal.azure.com -SendInvitationMessage $true
     New-AzureRmResourceGroup -Name $rgname -Location $location
-    New-AzureADGroup -DisplayName $rgname -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet"
+    #New-AzureADGroup -DisplayName $rgname -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet"
 
     $group = Get-AzureADGroup -SearchString $rgname | Select-Object ObjectId
 
-    New-AzureRmRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionName Contributor -ResourceGroupName $rgname
+    New-AzureRmRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionName Owner -ResourceGroupName $rgname
 
-    #New-AzStorageAccount -Name $uniqname -ResourceGroupName $rgname -Location $location -SkuName Standard_LRS -Kind StorageV2
+    New-AzureRmStorageAccount -ResourceGroupName $rgname -Name $storage -Location $location -SkuName Standard_LRS -kind StorageV2
 }
 $email = Read-Host -Prompt 'Input email'
 $name = Read-Host -Prompt 'Input name'
