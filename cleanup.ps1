d58a07e2746e12b84cf1c6dd574147e781fa6886
Jesper Berth <jesper.berth@arrow.com>
2021-03-04 10:37:32 +0100

    test

--- cleanup.ps1
+++ cleanup.ps1
@@ -6,23 +6,25 @@
 #
 # Author: Jesper Berth, jesper.berth@arrow.com - march 2021
 
-get-azureaduser | Where-Object UserPrincipalName -Match "^user.*"
+$azureaduser = get-azureaduser | Where-Object UserPrincipalName -Match "^user.*"
+
+Write-Host $azureaduser.UserPrincipalName
 
 #remove-azureaduser
 
-Get-AzureADApplication | Where-Object DisplayName -Match "*.^ansible.*" | Select-Object DisplayName
+#Get-AzureADApplication | Where-Object DisplayName -Match "*.^ansible.*" | Select-Object DisplayName
 
 #remove-azureadapplication
 
-Get-AzResourceGroup | Where-Object ResourceGroupName -Match "^user.*" | Select-Object ResourceGroupName
+#Get-AzResourceGroup | Where-Object ResourceGroupName -Match "^user.*" | Select-Object ResourceGroupName
 
 #Remove-AzResourceGroup
 
-Get-AzResourceGroup | Where-Object ResourceGroupName -Match "^ansible.*" | Select-Object ResourceGroupName
+#Get-AzResourceGroup | Where-Object ResourceGroupName -Match "^ansible.*" | Select-Object ResourceGroupName
 
 #Remove-AzResourceGroup
 
-Get-AzResourceGroup | Where-Object ResourceGroupName -Match "^webserver.*" | Select-Object ResourceGroupName
+#Get-AzResourceGroup | Where-Object ResourceGroupName -Match "^webserver.*" | Select-Object ResourceGroupName
 
 #Remove-AzResourceGroup
 
