33366618dba1d9b546bac8823c12b8aad825892b
Jesper Berth <jesper.berth@arrow.com>
2023-06-16 12:56:30 +0200

    adding ""

--- setup_azure.sh
+++ setup_azure.sh
@@ -26,7 +26,7 @@ create_users () {
         USERID=$(az ad user show --id user$1@$DOMAIN | jq -r .id)
         az role assignment create --assignee user$1@$DOMAIN --role Owner --scope /subscriptions/$SUBID
         echo $USERID
-        az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b '{"principalId":'+$USERID+',"roleDefinitionId":"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3","directoryScopeId":"/"}'
+        az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b '{"principalId":"'+$USERID+'","roleDefinitionId":"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3","directoryScopeId":"/"}'
     done
 }
 
