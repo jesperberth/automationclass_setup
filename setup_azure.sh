5e958118f7c9b841e7d48d8cdc6ccf6a651626a4
Jesper Berth <jesper.berth@arrow.com>
2023-06-16 14:02:20 +0200

    save

--- setup_azure.sh
+++ setup_azure.sh
@@ -31,31 +31,3 @@ create_users () {
 }
 
 create_users $NUMBERUSERS $PASSWORD
-
-
-# az ad user create --display-name user1 --password Hlk3@sdf!2 --user-principal-name user1@$DOMAIN
-
-# az role assignment create --assignee user1@$DOMAIN --role Owner --scope /subscriptions/$SUBID
-
-
-# az role assignment list --query "[?starts_with(name,'Application-')]
-
-
-# $Body = @{ 
-#     "roleDefinitionId" = "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3"; 
-#     "principalId"      = "a3c19bbf-f99a-42c7-b07f-fd54dc2409ae"; 
-#     "directoryScopeId" = "/"
-# } | ConvertTo-Json -Compress
-    
-# $Body = $Body.Replace('"', '\"')
-
-# az rest -m post -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b "$Body"
-
-# az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b {roleDefinitionId" = "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3"; "principalId" = "a3c19bbf-f99a-42c7-b07f-fd54dc2409ae";"directoryScopeId" = "/"}
-
-# az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b {\"principalId\":\"a3c19bbf-f99a-42c7-b07f-fd54dc2409ae\",\"roleDefinitionId\":\"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3\",\"directoryScopeId\":\"/\"}
-
-
-# Works with powershell
-
- #az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b '{"principalId":"$1","roleDefinitionId":"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3","directoryScopeId":"/"}' 
