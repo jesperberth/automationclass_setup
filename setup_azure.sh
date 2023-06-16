835f6d1d1a98e9daa1845c16e242b188bba9b71e
Jesper Berth <jesper.berth@arrow.com>
2023-06-16 12:44:21 +0200

    test user create

--- setup_azure.sh
+++ setup_azure.sh
@@ -8,32 +8,52 @@
 #
 # Author: Jesper Berth, jesper.berth@arrow.com - june 2023
 
-# LOCATION=$(az account list-locations --query '[].{Name:name,DisplayName:displayName}' | jq -s)
+DOMAIN=arrowadfs.onmicrosoft.com
+SUBID=$(az account list --query "[].{id:id}" -o tsv)
 
-# for LOC in $LOCATION
-#     do
-#         jq -r '.[].DisplayName' $LOC
-#         #echo $LOC
-#     done
+echo -e "How many users?\n"
+read NUMBERUSERS
 
-# echo Get Automationclass Container
-
-# CONTAINER=$(az container list --resource-group AutomationclassContainer | jq -r '.[].containers[].name')
-
-# echo - $CONTAINER
-
-# echo "Delete Container y/n"
-
-# read DELETECONTAINER
-
+echo -e "Enter default password for new users\n"
+read PASSWORD
 
 create_users () {
     echo -e "Creating $1 new users with password $2\n"
     for (( i=1 ; i<=$1 ; i++ ));
     do
         echo "Create - user$1"
+        az ad user create --display-name user$1 --password $2 --user-principal-name user$1@$DOMAIN
+        az role assignment create --assignee user$1@$DOMAIN --role Owner --scope /subscriptions/$SUBID
+        az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b '{"principalId":"$1","roleDefinitionId":"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3","directoryScopeId":"/"}'
     done
-    #az ad user create --display-name myuser --password password --user-principal-name myuser@contoso.com
 }
 
-create_users 4
+create_users $NUMBERUSERS $PASSWORD
+
+
+# az ad user create --display-name user1 --password Hlk3@sdf!2 --user-principal-name user1@$DOMAIN
+
+# az role assignment create --assignee user1@$DOMAIN --role Owner --scope /subscriptions/$SUBID
+
+
+# az role assignment list --query "[?starts_with(name,'Application-')]
+
+
+# $Body = @{ 
+#     "roleDefinitionId" = "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3"; 
+#     "principalId"      = "a3c19bbf-f99a-42c7-b07f-fd54dc2409ae"; 
+#     "directoryScopeId" = "/"
+# } | ConvertTo-Json -Compress
+    
+# $Body = $Body.Replace('"', '\"')
+
+# az rest -m post -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b "$Body"
+
+# az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b {roleDefinitionId" = "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3"; "principalId" = "a3c19bbf-f99a-42c7-b07f-fd54dc2409ae";"directoryScopeId" = "/"}
+
+# az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b {\"principalId\":\"a3c19bbf-f99a-42c7-b07f-fd54dc2409ae\",\"roleDefinitionId\":\"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3\",\"directoryScopeId\":\"/\"}
+
+
+# Works with powershell
+
+# az rest -m post --headers "Content-Type=application/json" -u "https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments" -b '{"principalId":"$1","roleDefinitionId":"9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3","directoryScopeId":"/"}' 
