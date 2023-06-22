4687c0f5b8f21fd3b9329a8f831478e19c9f0958
Jesper Berth <jesper.berth@arrow.com>
2023-06-22 14:17:38 +0200

    deploy tower

--- tower/deploy_tower.sh
+++ tower/deploy_tower.sh
@@ -16,14 +16,34 @@ echo -e "#################################\n"
 echo -e "How many instances?\n"
 read INSTANCES
 
+LIST=""
+VAR=""
+SERVERS=""
+PRE=tower
+POST="\n"
+
 for (( i=1 ; i<=$INSTANCES ; i++ ));
     do
-        echo -e "tower$i"
+        LIST+=$PRE$i$POST
     done
-
-# ansible-playbook -e "" 00_azure_tower_deploy.yml
+#echo $LIST
+VAR=$(printf $LIST)
+echo $VAR
+SERVERS=$(jq -n -c -M --arg var "$VAR" '{"servers": ($var|split("\n"))}')
+TOWER=\'$SERVERS\'
+ansible-playbook -e $TOWER 0_azure_tower_deploy.yml
 
 # ansible-galaxy install jesperberth.el_k3s -f
 # ansible-galaxy install jesperberth.awx_k8s_install -f
 
 # ansible-playbook -i inventory.azure_rm.yml install_awx.yml
+
+# LIST="tower1\ntower2\ntower3\n"
+
+
+# VAR=$(printf $LIST)
+# SERVERS=$(jq -n -c -M --arg var "$VAR" '{"servers": ($var|split("\n"))}')
+# echo \'$SERVERS\'
+
+
+
