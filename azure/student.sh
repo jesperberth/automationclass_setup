799b12bc935c12339d15922e40d223e656495a33
Jesper Berth <jesper.berth@arrow.com>
2024-11-20 20:53:36 +0100

    test gnome package

--- azure/student.sh
+++ azure/student.sh
@@ -27,7 +27,7 @@ echo $USER' ALL = NOPASSWD: /usr/bin/dockerd' | sudo EDITOR='tee -a' visudo
 # Install Gnome + xrdp
 #
 
-sudo apt-get install ubuntu-desktop task-gnome-desktop dconf-editor xrdp -y
+sudo apt-get install ubuntu-gnome-desktop dconf-editor xrdp -y
 
 sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
 
